450856290981a65f339fad7a52eed8e4c95f2d68
tinny0727 <152264236+tinny0727@users.noreply.github.com>
2026-05-02 14:40:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -310,7 +310,7 @@ function init() {
             const randomWord = allWords[Math.floor(Math.random() * allWords.length)];
             balls.push(new Ball({ word: randomWord }));
         }
-    }, 3000); 
+    }, 2000); 
 }
 
 window.onload = () => {
